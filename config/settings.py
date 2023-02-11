a21f5873be138510851cbef111e2e727288836cd
cyberthunderstorm <thunderhsot@gmail.com>
2023-02-11 15:20:20 +0200

    v0.0.1

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -53,9 +54,15 @@ MIDDLEWARE = [
 ]
 
 GRAPHENE = {
-    'SCHEMA': 'hackernews.schema.schema',
+    'SCHEMA': 'config.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
-
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
--- links/models.py
+++ links/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core.models import TimestampedModel
+from django.conf import settings
 
-# Create your models here.
+class Link(TimestampedModel):
+    
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    url = models.URLField()
+    description = models.TextField()
+class Vote(TimestampedModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    link = models.ForeignKey('links.Link', related_name='votes', on_delete=models.CASCADE)
